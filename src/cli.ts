82dce3c840e3be52c99cd5e3bd48685ac1759021
Ashley Davis <ashley@codecapers.com.au>
2022-03-21 10:02:44 +1000

    Fixed formatting.

--- src/cli.ts
+++ src/cli.ts
@@ -3,14 +3,14 @@ import { MockApi } from './index';
 const port = 6000;
 
 async function main(): Promise<void> {
-  const mockApi = new MockApi();
-  await mockApi.start(port);
+    const mockApi = new MockApi();
+    await mockApi.start(port);
 
-  console.log(`Mock API started at http://localhost:${port}`);
-  mockApi.displayFixtures();
+    console.log(`Mock API started at http://localhost:${port}`);
+    mockApi.displayFixtures();
 }
 
 main().catch((err) => {
-  console.error(`Failed with error:`);
-  console.error((err && err.stack) || err);
+    console.error(`Failed with error:`);
+    console.error((err && err.stack) || err);
 });
